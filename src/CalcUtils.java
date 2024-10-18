09af2c845ecd353746a29fab155283a1bdc2919a
wakeisle9933 <wakeisle9933@gmail.com>
2024-10-18 09:58:03 +0900

    Commit for AI review generation

---
+++ src/CalcUtils.java
@@ -0,0 +1,9 @@
+public class CalcUtils {
+
+  public int addNumber(int i, int j) {
+    System.out.println("Remove this");
+    System.out.println("i : " + i + ", j : " + j);
+    return i + j;
+  }
+
+}
